087c234d3b2e5431435788486115cffbca17672c
Szczur3k <mateuszlukaszczyk13@gmail.com>
2021-03-24 20:06:02 +0100

    working mapper for one Repo

---
+++ src/main/java/pl/mateuszlukaszczyk/gitRepositories/models/GitOwner.java
@@ -0,0 +1,2 @@
+package pl.mateuszlukaszczyk.gitRepositories.models;public class GitOwner {
+}
---
+++ src/main/java/pl/mateuszlukaszczyk/gitRepositories/services/dto/GitOwnerMapper.java
@@ -0,0 +1,2 @@
+package pl.mateuszlukaszczyk.gitRepositories.services.dto;public class GitOwnerMapper {
+}
